e364283f8643aaba48e1d8627906c9038bcecefe
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2022-08-23 18:02:37 +0200

    Minor fixes

--- Assets/Scripts/SAE_Portfolio_2/Player/Player.cs
+++ Assets/Scripts/SAE_Portfolio_2/Player/Player.cs
@@ -1,9 +1,11 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace SAE_Portfolio_2.Player
 {
+    //Only used for identifying the player's GameObject
     public sealed class Player : MonoBehaviour
     {
 
